66398ff49c1605de9429db22face9fec3faf2982
Alejandro Metke <ametke@gmail.com>
2013-06-01 15:13:54 +1000

    Added log files.

--- src/main/java/org/govhack/rest/RootResource.java
+++ src/main/java/org/govhack/rest/RootResource.java
@@ -28,11 +28,11 @@ public class RootResource {
 	@GET
 	@Path("observations/{year}/{month}/{day}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Map<String, List<Observation>> getMsg(@PathParam("year") String year, @PathParam("month") String month, 
+	public Map<String, List<Observation>> getObservations(@PathParam("year") String year, @PathParam("month") String month, 
 			@PathParam("day") String day) throws IOException, ParseException {
 		
 		LogParser lp = new LogParser();
-		String file = "/Museum Qld "+day+"-"+month+"-"+year+" Camera.txt";
+		String file = "/Museum Qld "+day+"-"+month+"-"+year+".log";
 		InputStream in = this.getClass().getResourceAsStream(file);
 		if(in != null) {
 			return lp.getNormalisedObservationsMap(in);
@@ -40,6 +40,16 @@ public class RootResource {
 		
 		return null;
  
+	}
+	
+	public static void main (String[] args) throws IOException, ParseException {
+		RootResource rr = new RootResource();
+		Map<String, List<Observation>> res = rr.getObservations("2013", "01", "01");
+		for(String key : res.keySet()) {
+			for(Observation obs : res.get(key)) {
+				System.out.println(obs);
+			}
+		}
 	}
 	
 }
